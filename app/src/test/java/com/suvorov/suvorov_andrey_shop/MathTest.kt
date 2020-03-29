413bd682cf4cb022a34a2a8911d2f155f16a3c1a
Suvorov <als_2991@mail.ru>
2020-03-29 13:28:15 +0500

    add Test "MathTest"

---
+++ app/src/test/java/com/suvorov/suvorov_andrey_shop/MathTest.kt
@@ -0,0 +1,15 @@
+package com.suvorov.suvorov_andrey_shop
+
+import org.junit.Assert.assertEquals
+import org.junit.Test
+
+class MathTest{
+
+    @Test
+    fun  main(){
+        var i = 1
+        i += 1
+
+        assertEquals(2,i)
+    }
+}
