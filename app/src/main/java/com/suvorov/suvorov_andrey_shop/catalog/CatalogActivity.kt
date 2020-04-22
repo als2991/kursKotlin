a29301c72acd8cf8f9810be2bbff54d3d1b41b85
Suvorov <als_2991@mail.ru>
2020-04-22 09:19:49 +0500

    Lessons 10 start

--- app/src/main/java/com/suvorov/suvorov_andrey_shop/catalog/CatalogActivity.kt
+++ app/src/main/java/com/suvorov/suvorov_andrey_shop/catalog/CatalogActivity.kt
@@ -4,16 +4,28 @@ import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
 import android.util.Log
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.suvorov.suvorov_andrey_shop.BaseActivity
 import com.suvorov.suvorov_andrey_shop.R
 import com.suvorov.suvorov_andrey_shop.about_product.AboutProductActivity
 import com.suvorov.suvorov_andrey_shop.basket.BasketActivity
 import kotlinx.android.synthetic.main.catalog_layout.*
 
-class CatalogActivity: BaseActivity() {
+class CatalogActivity: BaseActivity(), CatalogView {
+
+    private val presenter = CatalogPresenter()
+    private val adapter = CategoryAdapter { category ->
+        presenter.removeItem(category)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.catalog_layout)
+
+        //example when save value in Bundle
+        Log.d(tag, "saveInstanceState = $savedInstanceState")
+        val savedInt = savedInstanceState?.getInt(SAVE_STATE_INT)
+        Log.d(tag, "saveInd = $savedInt")
 
         // working variant with example putExtra
         catalogBasketImg.setOnClickListener {
@@ -27,8 +39,22 @@ class CatalogActivity: BaseActivity() {
         val intent = Intent(this, AboutProductActivity::class.java )
         startActivity(intent)
         }
+
+        categoryRv.layoutManager = LinearLayoutManager(this)
+        categoryRv.adapter = adapter
+
+        presenter.attachView(this)
+        presenter.setData()
+
     }
 
+    override fun setCategories(list: List<String>) {
+        adapter.setData(list)
+    }
+
+    override fun removeItem(position: Int) {
+        adapter.notifyItemRemoved(position)
+    }
 
 
 
---
+++ app/src/main/java/com/suvorov/suvorov_andrey_shop/catalog/CatalogPresenter.kt
@@ -0,0 +1,26 @@
+package com.suvorov.suvorov_andrey_shop.catalog
+
+import moxy.MvpPresenter
+
+class CatalogPresenter : MvpPresenter<CatalogView>(){
+
+    private val list = mutableListOf(
+        "Телевизоры",
+        "Телефоны",
+        "Планшеты",
+        "Часы",
+        "Компьютеры",
+        "Ноутбуки"
+    )
+
+    fun setData(){
+        viewState.setCategories(list)
+    }
+
+    fun removeItem(category: String){
+        val position = list.indexOf(category)
+        list.remove(category)
+        viewState.removeItem(position)
+    }
+
+}
