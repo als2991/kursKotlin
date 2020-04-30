f44f76bcc309a3f253c5d347e76a697713ed2eed
Suvorov <als_2991@mail.ru>
2020-05-01 01:38:07 +0500

    Lessons 12

--- app/src/main/java/com/suvorov/suvorov_andrey_shop/domain/MainApi.kt
+++ app/src/main/java/com/suvorov/suvorov_andrey_shop/domain/MainApi.kt
@@ -3,23 +3,25 @@ package com.suvorov.suvorov_andrey_shop.domain
 import retrofit2.http.GET
 import retrofit2.http.Path
 
+data class RemoteProduct(
+    val id: String,
+    val name: String,
+    val price: Double,
+    val discountPercent: Int,
+    val description: String,
+    val imageUrl: String,
+    val attributes: List<Attribute>
+) {
+    data class Attribute(
+        val name: String,
+        val value: String
+    )
+}
+
 interface MainApi {
 
-    data class RemoteProduct(
-        val id: String,
-        val name: String,
-        val price: Double,
-        val discountPercent: Int,
-        val description: String,
-        val imageUrl: String,
-        val attributes: List<Attribute>
-    ) {
-        data class Attribute(
-            val name: String,
-            val value: String
-        )
-    }
-
-    @GET("product/all/{author}")
+    @GET("products/all/{author}")
     suspend fun allProduct(@Path("author") author: String): List<RemoteProduct>
 }
+
+
